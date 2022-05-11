23886af8b11c7a2f4b144870002dcce06f4ea856
leonardopellegrini-github <leonardo.giulio.pellegrini@gmail.com>
2022-05-11 17:49:03 +0200

    correzione con v-for

--- script.js
+++ script.js
@@ -4,15 +4,17 @@ const app = new Vue({
 
   data:{
     nomeUtente: "",
+    isLoading: true,
   },
 
   methods:{
     stampa10(){
       for( let i = 0; i < 10; i++ ){
+    
         axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
         .then(res =>{
         console.log(res.data.response);
-        this.nomeUtente += res.data.response + " "
+        this.nomeUtente += res.data.response + "   ";
         })
       }
     }
