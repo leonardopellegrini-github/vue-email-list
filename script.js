4f4b4a3a460149dfd1a7d13fb0bf8a5252a3b84f
leonardopellegrini-github <leonardo.giulio.pellegrini@gmail.com>
2022-05-11 16:24:08 +0200

    inserimento mail in pagina

--- script.js
+++ script.js
@@ -3,13 +3,23 @@ const app = new Vue({
   el: '#app',
 
   data:{
-
+    nomeUtente: "",
   },
 
-
+  methods:{
+    stampa10(){
+      for( let i = 0; i < 10; i++ ){
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then(res =>{
+        console.log(res.data.response);
+        this.nomeUtente += res.data.response + " "
+        })
+      }
+    }
+  },
 
   mounted(){
-    
+    this.stampa10();
   }
 
-}),
+})
